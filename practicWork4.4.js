2f972d0b8db5db5e212f9a817be3fe16d8881b3c
Anna <she13lest@gmail.com>
2022-12-18 15:37:24 +0100

    new

--- practicWork4.4.js
+++ practicWork4.4.js
@@ -2,20 +2,17 @@ let x = [3, 10];
 let y = [];
 x[77] = 7;
 
-// for(let i = 0; i < x.length; i++) {
-//     y.push(Math.pow((x[i]), 2))
-// }
-// console.log(y)
+for(let i = 0; i < x.length; i++) {
+    y.push(Math.pow((x[i]), 2))
+}
 
-// for(let val in x) {
-//     y.push(Math.pow((x[val]), 2))
-// }
-// console.log(y)
+
+for(let val in x) {
+    y.push(Math.pow((x[val]), 2))
+}
+
 
 for(let val of x) {
     y.push(Math.pow(val, 2))
 }
 console.log(y)
-
-
-
