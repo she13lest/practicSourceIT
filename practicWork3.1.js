89f0f370509a02bde4d5657e52aa83cdf6aba063
Anna <she13lest@gmail.com>
2022-12-26 16:00:02 +0100

    new

--- practicWork3.1.js
+++ practicWork3.1.js
@@ -30,12 +30,28 @@ let res = '';
 const printRow = (numSpace, numStar, symbSpace, symbStar) => {
     res = '';
 
+ 
+    for(let k = 1; k <= numSpace; k++){
+        res += symbSpace;
+    
+    }
+
+    
+    for(let k = 1; k <= numStar; k++){
+        res += symbStar;
+    }
+}
+
+for(let i = 1; i <= rows; i++){
+    let numSpace = rows - i;
+    let numStar = i * 2 - 1;
+    printRow(numSpace, numStar, ' ', '$')
+   
     console.log(res)
 }
 
-for(let i =1; i <= rows; i ++) {
-spaces = rows - i
 
-    printRow( , , ' ', '*')
-}
+
+
+
 
